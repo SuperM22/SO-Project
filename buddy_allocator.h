131f0bceb83c97440bd9205e550e2d2f8db67c69
SuperM22 <domece90@gmail.com>
2022-03-15 20:42:29 +0100

    inizializzazione

---
+++ buddy_allocator.h
@@ -0,0 +1,26 @@
+#pragma once
+#include "bitmap.h"
+
+#define MAX_LEVELS 16
+
+typedef struct  {
+  BitMap bitmap;
+  int num_levels;
+  char* buffer; // the buffer area to be managed
+  int buffer_size;
+  int min_bucket_size; // the minimum page of RAM that can be returned
+} BuddyAllocator;
+
+// initializes the buddy allocator, and checks that the buffer is large enough
+void BuddyAllocator_init(BuddyAllocator* alloc,
+                         int num_levels,
+                         char* buffer,
+                         int buffer_size,
+                         char* memory,
+                         int min_bucket_size);
+
+//allocates memory
+void* BuddyAllocator_malloc(BuddyAllocator* alloc, int size);
+
+//releases allocated memory
+void BuddyAllocator_free(BuddyAllocator* alloc, void* mem);
